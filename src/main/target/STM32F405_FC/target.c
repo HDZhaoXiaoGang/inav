eeffcff2c953afbe1b475032d8b50ebf9bee497d
zhaoxiaogang <zhaoxiaogang@zhihu.com>
2025-08-05 20:54:52 +0800

    f405自定义飞控第一版

---
+++ src/main/target/STM32F405_FC/target.c
@@ -0,0 +1,48 @@
+/*
+* This file is part of Cleanflight.
+*
+* Cleanflight is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Cleanflight is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdbool.h>
+#include <platform.h>
+#include "drivers/bus.h"
+#include "drivers/io.h"
+#include "drivers/pwm_mapping.h"
+#include "drivers/timer.h"
+#include "drivers/pinio.h"
+
+timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM3,  CH1, PB3,  TIM_USE_OUTPUT_AUTO, 0, 0), // S1  D(1,4,5)
+    DEF_TIM(TIM3,  CH2, PB4,  TIM_USE_OUTPUT_AUTO, 0, 0), // S2  D(1,5,5)
+    DEF_TIM(TIM3,  CH3, PB5,  TIM_USE_OUTPUT_AUTO, 0, 0), // S3  D(1,6,5)
+    DEF_TIM(TIM4,  CH1, PB8,  TIM_USE_OUTPUT_AUTO, 0, 0), // S4  D(1,0,2)
+    DEF_TIM(TIM4,  CH2, PB9,  TIM_USE_OUTPUT_AUTO, 0, 0), // S5  D(1,3,2)
+    DEF_TIM(TIM2,  CH1, PA0,  TIM_USE_OUTPUT_AUTO, 0, 0), // S6  D(1,2,3)
+    DEF_TIM(TIM2,  CH2, PA1,  TIM_USE_OUTPUT_AUTO, 0, 0), // S7  D(1,3,3)
+
+    // 备用电机控制端口
+    DEF_TIM(TIM5,  CH1, PB0,  TIM_USE_ANY, 0, 0), // MS1
+    DEF_TIM(TIM5,  CH2, PB1,  TIM_USE_ANY, 0, 0), // MS2
+    DEF_TIM(TIM8,  CH4, PC9,  TIM_USE_ANY, 0, 0), // MS3
+    DEF_TIM(TIM1,  CH1, PA8,  TIM_USE_ANY, 0, 0), // MS4
+
+    // UART引脚
+    DEF_TIM(TIM5,  CH3, PA2,  TIM_USE_ANY, 0, 0), // UART2_TX
+    DEF_TIM(TIM5,  CH4, PA3,  TIM_USE_ANY, 0, 0), // UART2_RX
+    DEF_TIM(TIM4,  CH3, PB10, TIM_USE_ANY, 0, 0), // UART3_TX
+    DEF_TIM(TIM4,  CH4, PB11, TIM_USE_ANY, 0, 0), // UART3_RX
+};
+
+const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]); 
